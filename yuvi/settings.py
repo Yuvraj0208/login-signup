d6f1b435c343c741eb5e6ccca0833b7aed2c26a5
Yuvraj0208 <syuvraj61@gmail.com>
2023-06-29 02:49:37 +0530

    new file added

--- yuvi/settings.py
+++ yuvi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%f)#7+$#-$3(0t1e=!hse00)n8^eldk8t)cjlp)-o!_m-a$p%e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -121,9 +121,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
